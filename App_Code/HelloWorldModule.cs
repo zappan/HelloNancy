5caacbc41aaf3d0002d0b016cea5695bbac2cb2b
Tomislav Capan <tomislav@netko.hr>
2011-03-03 21:45:34 +0100

    Hello Nancy initial commit

---
+++ App_Code/HelloWorldModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Nancy;
+
+/// <summary>
+/// Summary description for HelloWorldModule
+/// </summary>
+public class HelloWorldModule : NancyModule
+{
+	public HelloWorldModule()
+	{
+		Get["/"] = parameters => {
+			return "Hello, Nancy!";
+		};
+		
+		Get["/test"] = x => {
+			return "This is the Test route";
+		};
+	}
+}
