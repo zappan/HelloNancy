672e7281ebc17bfdd26c9c99ee82c30e2342fc01
Tomislav Capan <tomislav@netko.hr>
2011-03-06 19:36:33 +0100

    Simple.Data configured and run through Nancy backed with Sql Server Compact 4

--- App_Code/HelloWorldModule.cs
+++ App_Code/HelloWorldModule.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using System.Web;
 using Nancy;
+using Simple.Data;
 
 /// <summary>
 /// Summary description for HelloWorldModule
 /// </summary>
 public class HelloWorldModule : NancyModule
 {
+    private static readonly string DbPath = System.IO.Path.Combine
+				( System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory)
+				, @"App_Data\HelloNancy.sdf");
+
+	
 	public HelloWorldModule()
 	{
 		Get["/"] = parameters => {
 			return "Hello, Nancy!";
 		};
 		
-		Get["/test"] = x => {
+		Get["/test"] = parameters => {
 			return "This is the Test route";
 		};
 		
-		Get["/spark"] = x => {
-			var model = new Person { FirstName = "Nancy", LastName = "Spark" };
+		Get["/spark"] = parameters => {
+			var db = Database.OpenFile(DbPath);
+			Person model = db.Persons.FindById(1);
+			if (model == null)
+				model = new Person { FirstName = "Jane", LastName = "Doe" };
 			return View["spark", model];
 		};
 	}
--- App_Code/Models/Person.cs
+++ App_Code/Models/Person.cs
@@ -5,8 +5,9 @@ using System.Web;
 /// <summary>Person model</summary>
 public class Person
 {
+	public long Id { get; set; }
 	public string FirstName { get; set; }
 	public string LastName { get; set; }
 	
-	public string FullName { get { return string.Format("{0} {1}", FirstName, LastName); } }
+	public string FullName { get { return string.Format("{0} {1}", FirstName, LastName).Trim(); } }
 }
